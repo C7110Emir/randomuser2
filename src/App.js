7315c75f1cd0881e937f0c0db75facf5d2b5d17f
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-14 11:32:49 -0400

    edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Wholepage from "./components/wholepage"
+import style from"./App.module.css"
+import cw from "./components/images/cw.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={style.app}>
+      <header className={style.imagediv}>
+        <img src={cw} alt="logo" className={style.image}/>
       </header>
+      <content>
+      <Wholepage></Wholepage>
+      </content>
+      
     </div>
   );
 }
---
+++ src/components/wholepage.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./wholepage.css"
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import growingupman from "./images/growing-up-man.svg"
+import growingupwomen from "./images/growing-up-woman.svg"
+import mail from "./images/mail.svg"
+import man from "./images/man.svg"
+import map from "./images/map.svg"
+import padlock from "./images/padlock.svg"
+import phone from "./images/phone.svg"
+import woman from "./images/woman.svg"
+
+export default function Wholepage() {
+    const [data,setData] = useState([])
+    const [text,setText] = useState("My Name is")
+    const [currentData, setCurrentData] = useState()
+    const getData = () =>{
+        axios.get("https://randomuser.me/api/").then((res)=>{
+            setData(res.data.results)
+            setCurrentData(res.data.results[0].name.title + " " + res.data.results[0].name.first + " " + res.data.results[0].name.last)
+        })
+    }
+    useEffect(()=>{
+        getData();
+    },[])
+    
+    
+    return (
+        <div className="outerDiv">
+            
+
+            {data?.map((element)=>{
+
+                return(
+                    <div className="imagediv">
+                        <img src={element.picture.large} className="image" alt="userimage"/><br/><hr/><br/>
+                        
+                        <div className="datatext">{text}<br/><br/>{currentData}</div>
+                    </div>
+                )
+            })}
+        </div>
+
+    )
+}
