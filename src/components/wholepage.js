93248bb442dfed338d633b166bd80b8df5b59e6b
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-15 21:33:46 -0400

    edits

--- src/components/wholepage.js
+++ src/components/wholepage.js
@@ -31,13 +31,23 @@ export default function Wholepage() {
             
 
             {data?.map((element)=>{
+                console.log(element)
 
                 return(
-                    <div className="imagediv">
+                    <div className="contentdiv">
+                    <div className="imagentext">
                         <img src={element.picture.large} className="image" alt="userimage"/><br/><hr/><br/>
                         
                         <div className="datatext">{text}<br/><br/>{currentData}</div>
+                        </div>
+                    <div className="hoverimages">
+                        <img src={element.gender == "male" ? man : woman} alt="loading" className="img" onMouseOver={() => {setText("My Name Is") ; setCurrentData(element.name.title + " " + element.name.first + " " + element.name.last)}}/>
+                        <img src={mail} className="img" alt="loading" onMouseOver={()=>{setText("My Email Is") ; setCurrentData(element.email)}}/>
+                        <img src={element.gender == "male" ? growingupman : growingupwomen} className="img" onMouseOver={()=>{setText("My Age Is"); setCurrentData(element.dob.age)}}/>
+                        <img src={map} className="img" alt="loading" onMouseOver={()=>{setText("My Street Address is"); setCurrentData(element.location.street.number +" " + element.location.street.name)}}/>
                     </div>
+                    </div>
+                        
                 )
             })}
         </div>
